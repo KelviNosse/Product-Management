0b23e685dcf2add85bb1401f4c9ca76e0ac05350
Kelvin Nose <kelvinjosse@gmail.com>
2016-02-08 21:12:32 -0600

    Changed url to .net api url

--- js/services.js
+++ js/services.js
@@ -1,7 +1,7 @@
 var app = angular.module('ProductsApp');
 
 //Temporary url
-var url = 'http://kelvinosse.github.io/Product-Management/api/products.json';
+var url = 'http://planillaanal.apphb.com/api/Products';
 
 app.factory('Product', ($resource) => {
     return $resource(url, {} , {

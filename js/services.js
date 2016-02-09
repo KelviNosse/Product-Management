76da92098187c9fb81b1a5a48fb68196f363f42d
Kelvin Nose <kelvinjosse@gmail.com>
2016-02-08 21:03:04 -0600

    Changed temporary url

--- js/services.js
+++ js/services.js
@@ -1,7 +1,7 @@
 var app = angular.module('ProductsApp');
 
 //Temporary url
-var url = 'http://localhost:8080/api';
+var url = 'http://kelvinosse.github.io/Product-Management/api/products.json';
 
 app.factory('Product', ($resource) => {
     return $resource(url, {} , {

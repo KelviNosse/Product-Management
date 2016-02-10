3a34b40e282e93f779055e9f552e254502d82e9b
Kelvin Nose <kelvinjosse@gmail.com>
2016-02-09 22:49:47 -0600

    Connection to backend nodejs server

--- js/services.js
+++ js/services.js
@@ -1,7 +1,7 @@
 var app = angular.module('ProductsApp');
 
 //Temporary url
-var url = 'http://planillaanal.apphb.com/api/Products';
+var url = 'http://productmonitor.herokuapp.com';
 
 app.factory('Product', ($resource) => {
     return $resource(url, {} , {
